79f43a3fc15c9cb8dc3d572d97a2b4fe6ff1ee20
Navinkumar Boddu <nbod@softwareag.com>
2023-09-05 10:57:48 +0530

    Close #2 Added Method References, Lambdas and Filters

---
+++ src/main/java/in28Minutes/fpwj/coding/FP01Functional.java
@@ -0,0 +1,44 @@
+package in28Minutes.fpwj.coding;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FP01Functional {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+		printAllNumbersFunctionalApproach(numbers);
+		printAllEvenNumbers(numbers);
+	}
+
+	private static void print(Integer number) {
+		System.out.println(number);
+	}
+
+	private static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+
+	private static void printAllNumbersFunctionalApproach(List<Integer> numbers) {
+		System.out.println("All Numbers");
+		numbers.stream()
+			.forEach(FP01Functional::print); // Method Reference
+
+		// Using sysout Method Reference
+		numbers.stream()
+			.forEach(System.out::println);
+	}
+
+	private static void printAllEvenNumbers(List<Integer> numbers) {
+		System.out.println("Even Numbers");
+		numbers.stream()
+		 	.filter(FP01Functional::isEven) // Filter Even Numbers
+		 	.forEach(System.out::println);
+		
+		//Lambda Expression
+		numbers.stream()
+			.filter(n -> n%2 == 0)
+			.forEach(System.out::println);
+	}
+}
--- src/main/java/in28Minutes/fpwj/coding/FP01Imperative.java
+++ src/main/java/in28Minutes/fpwj/coding/FP01Imperative.java
@@ -4,27 +4,31 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class FP01Exercise {
+public class FP01Imperative {
 	public static void main(String[] args) {
 		List<Integer> numbers =  Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15);
 		
 		printAllNumbers(numbers);
-		printAllNumbersFunctionalApproach(numbers);
+		printAllEvenNumbers(numbers);
 	}
 	
-	private static void print(Integer number) {
-		System.out.println(number);
-	}
-
 	private static void printAllNumbers(List<Integer> numbers) {
 		// for each loop
+		System.out.println("All Numbers");
 		for (Integer number : numbers) {
 			System.out.println(number);
 		}
 	}
 	
-	private static void  printAllNumbersFunctionalApproach(List<Integer> numbers) {
-		Stream<Integer> s = numbers.stream();
-		s.forEach(FP01Exercise::print);
+	private static void printAllEvenNumbers(List<Integer> numbers) {
+		System.out.println("Even Numbers:");
+		for (Integer number : numbers) {
+			if(number % 2 == 0) {
+				System.out.println(number);
+			}
+		}
 	}
+
+	
+	
 }
