341867baa13248ad4715f08442cac672fc9acf07
Navinkumar Boddu <nbod@softwareag.com>
2023-09-05 13:14:51 +0530

    Close #9 Print the courses and length

--- src/main/java/in28Minutes/fpwj/coding/FP01Exercise.java
+++ src/main/java/in28Minutes/fpwj/coding/FP01Exercise.java
@@ -7,21 +7,25 @@ public class FP01Exercise {
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15);
 
+		//#Exercise 1
 		printAllOddNumbers(numbers);
 		
 		List<String> courses = Arrays.asList("Spring", "Spring Boot", "API" , "Microservices","AWS", "PCF","Azure", "Docker", "Kubernetes");
 
+		//#Exercise 2
 		//print courses
 		System.out.println("Print courses");
 		courses.stream()
 			.forEach(System.out::println);
 		
+		//#Exercise 3
 		//print courses containing Spring
 		System.out.println("Print courses containing Spring");
 		courses.stream()
 			.filter(c -> c.toLowerCase().contains("Spring".toLowerCase()))
 			.forEach(System.out::println);
 		
+		//#Exercise 4
 		//print courses length >= 4
 		System.out.println("print courses length >= 4");
 		courses.stream()
@@ -30,6 +34,12 @@ public class FP01Exercise {
 		
 		//#Exercise 5
 		printCubesOfOddNumbers(numbers);
+		
+		//#Exercise 6
+		System.out.println("Print courses length::");
+		courses.stream()
+			.map(c -> c + " " + c.length())
+			.forEach(System.out::println);
 	}
 
 	private static void printAllOddNumbers(List<Integer> numbers) {
