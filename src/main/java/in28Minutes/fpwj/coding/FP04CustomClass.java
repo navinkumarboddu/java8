558467bae52e75ca02daef36061af00b2ab32ceb
Navinkumar Boddu <nbod@softwareag.com>
2023-09-18 16:57:55 +0530

    Closes #24

--- src/main/java/in28Minutes/fpwj/coding/FP04CustomClass.java
+++ src/main/java/in28Minutes/fpwj/coding/FP04CustomClass.java
@@ -1,8 +1,10 @@
 package in28Minutes.fpwj.coding;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class Course {
 	private String name;
@@ -67,26 +69,41 @@ public class FP04CustomClass {
 				new Course("FullStack", "FullStack", 91, 14000), new Course("AWS", "Cloud", 92, 21000),
 				new Course("Azure", "Cloud", 99, 21000), new Course("Docker", "Cloud", 92, 20000),
 				new Course("Kubernetes", "Cloud", 91, 20000));
-	
-		//allMatch, noneMatch, anyMatch
-		
+
+		// allMatch, noneMatch, anyMatch
+
 		Predicate<? super Course> reviewScoreGreaterThan95Predicate = c -> c.getReviewScore() > 95;
-		
+
 		Predicate<? super Course> reviewScoreGreaterThan90Predicate = c -> c.getReviewScore() > 90;
-		
+
 		Predicate<? super Course> reviewScoreLessThan90Predicate = c -> c.getReviewScore() < 90;
+
+		System.out.println(courses.stream().allMatch(reviewScoreGreaterThan95Predicate));
+
+		System.out.println(courses.stream().noneMatch(reviewScoreLessThan90Predicate));
+
+		System.out.println(courses.stream().anyMatch(reviewScoreLessThan90Predicate));
+
+		System.out.println(courses.stream().anyMatch(reviewScoreGreaterThan95Predicate));
+
 		
-		System.out.println(courses.stream()
-				.allMatch(reviewScoreGreaterThan95Predicate));
+		/***Sorting and Comparing***/
+		Comparator<Course> comparingByNoOfStudentsIncreasing = Comparator.comparingInt(Course::getNoOfStudents);
+
+		Comparator<Course> comparingByNoOfStudentsDecreasing = Comparator.comparingInt(Course::getNoOfStudents).reversed();
+
+		System.out.println(courses.stream().sorted(comparingByNoOfStudentsIncreasing).collect(Collectors.toList()));
+		//[Course [name=FullStack, category=FullStack, reviewScore=91, noOfStudents=14000], Course [name=Spring Boot, category=Framework, reviewScore=95, noOfStudents=18000], Course [name=Spring, category=Framework, reviewScore=98, noOfStudents=20000], Course [name=Docker, category=Cloud, reviewScore=92, noOfStudents=20000], Course [name=Kubernetes, category=Cloud, reviewScore=91, noOfStudents=20000], Course [name=AWS, category=Cloud, reviewScore=92, noOfStudents=21000], Course [name=Azure, category=Cloud, reviewScore=99, noOfStudents=21000], Course [name=API, category=Microservices, reviewScore=97, noOfStudents=22000], Course [name=Microservices, category=Microservices, reviewScore=96, noOfStudents=25000]]
 		
-		System.out.println(courses.stream()
-				.noneMatch(reviewScoreLessThan90Predicate));
+		System.out.println(courses.stream().sorted(comparingByNoOfStudentsDecreasing).collect(Collectors.toList()));
+		//[Course [name=Microservices, category=Microservices, reviewScore=96, noOfStudents=25000], Course [name=API, category=Microservices, reviewScore=97, noOfStudents=22000], Course [name=AWS, category=Cloud, reviewScore=92, noOfStudents=21000], Course [name=Azure, category=Cloud, reviewScore=99, noOfStudents=21000], Course [name=Spring, category=Framework, reviewScore=98, noOfStudents=20000], Course [name=Docker, category=Cloud, reviewScore=92, noOfStudents=20000], Course [name=Kubernetes, category=Cloud, reviewScore=91, noOfStudents=20000], Course [name=Spring Boot, category=Framework, reviewScore=95, noOfStudents=18000], Course [name=FullStack, category=FullStack, reviewScore=91, noOfStudents=14000]]
 		
-		System.out.println(courses.stream()
-			.anyMatch(reviewScoreLessThan90Predicate));
-		
-		System.out.println(courses.stream()
-				.anyMatch(reviewScoreGreaterThan95Predicate));
+		Comparator<Course> comparingByNoOfStudentsAndNoOfReviewsDecreasing = Comparator.comparing(Course::getNoOfStudents).thenComparingInt(Course::getReviewScore).reversed();
+
+		System.out.println(courses.stream().sorted(comparingByNoOfStudentsAndNoOfReviewsDecreasing).collect(Collectors.toList()));
+		//[Course [name=Microservices, category=Microservices, reviewScore=96, noOfStudents=25000], Course [name=API, category=Microservices, reviewScore=97, noOfStudents=22000], Course [name=Azure, category=Cloud, reviewScore=99, noOfStudents=21000], Course [name=AWS, category=Cloud, reviewScore=92, noOfStudents=21000], Course [name=Spring, category=Framework, reviewScore=98, noOfStudents=20000], Course [name=Docker, category=Cloud, reviewScore=92, noOfStudents=20000], Course [name=Kubernetes, category=Cloud, reviewScore=91, noOfStudents=20000], Course [name=Spring Boot, category=Framework, reviewScore=95, noOfStudents=18000], Course [name=FullStack, category=FullStack, reviewScore=91, noOfStudents=14000]]
+
+		/***Sorting and Comparing***/
 	}
 
 }
