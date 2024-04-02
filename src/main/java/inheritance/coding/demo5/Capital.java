184c7ae92aaabdbbab900b6568577d0fd935cefc
Navinkumar Boddu <swarmrobots@gmail.com>
2024-04-02 21:08:27 +0530

    added super() example

---
+++ src/main/java/inheritance/coding/demo5/Capital.java
@@ -0,0 +1,14 @@
+package inheritance.coding.demo5;
+
+import inheritance.coding.demo5.City;
+
+public class Capital extends City {
+
+    public Capital() {
+        super();
+    }
+
+    public Capital(String name) {
+        super(name);
+    }
+}
