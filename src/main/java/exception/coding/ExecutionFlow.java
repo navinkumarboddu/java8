02a9a5ec8a0b1a4a27cfc111d2f770779a4c6370
Navinkumar Boddu <swarmrobots@gmail.com>
2024-04-03 23:59:05 +0530

    updated Exception ExecutionFlow example

--- src/main/java/exception/coding/ExecutionFlow.java
+++ src/main/java/exception/coding/ExecutionFlow.java
@@ -12,5 +12,20 @@ public class ExecutionFlow {
         }
         sb.append("d");
         System.out.println(sb);
+
+        sb = new StringBuilder();
+        String str = null;
+        try {
+            sb.append("a");
+            str.toUpperCase();
+            sb.append("b");
+        } catch(IllegalArgumentException ie){
+            sb.append("c");
+        } catch (Exception e){
+            sb.append("d");
+        } finally {
+            sb.append("e");
+        }
+        System.out.println(sb);
     }
 }
