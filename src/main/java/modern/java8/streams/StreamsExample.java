5c8256b2d0d06cd34313a97c096860ed2ca75c10
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-17 10:05:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamsExample.java
+++ src/main/java/modern/java8/streams/StreamsExample.java
@@ -5,6 +5,7 @@ import modern.java8.data.StudentDataBase;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StreamsExample {
@@ -12,8 +13,13 @@ public class StreamsExample {
     public static void main(String[] args) {
         //student and list of activities in a map
         Map<String, List<String>> studentMap = StudentDataBase.getAllStudents().stream()
+                .filter(studentGradePredicate)
+                .filter(getStudentGpaPredicate)
                 .collect(Collectors.toMap(Student::getName, Student::getActivities));
 
         System.out.println(studentMap);
     }
+
+    static Predicate<Student> studentGradePredicate = (student -> student.getGradeLevel() >= 3);
+    static Predicate<Student> getStudentGpaPredicate = (student -> student.getGpa() >= 3.9);
 }
