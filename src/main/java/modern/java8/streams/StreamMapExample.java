c61e5b4eb9187ffd774d88548f4f60a4cae1d062
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-18 10:10:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamMapExample.java
+++ src/main/java/modern/java8/streams/StreamMapExample.java
@@ -4,6 +4,7 @@ import modern.java8.data.Student;
 import modern.java8.data.StudentDataBase;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamMapExample {
@@ -18,9 +19,20 @@ public class StreamMapExample {
                 .collect(Collectors.toList());
         return studentList;
     }
+
+    public static Set<String> namesSet(){
+        Set<String> studentSet =
+                StudentDataBase.getAllStudents()
+                        .stream() //Stream<Student>
+                        //Student as an input -> Student name
+                        .map(Student::getName) //Stream<String>
+                        .map(String::toUpperCase) //uppercase operation
+                        .collect(Collectors.toSet());
+        return studentSet;
+    }
     public static void main(String[] args) {
 
         System.out.println(namesList());
-
+        System.out.println(namesSet());
     }
 }
