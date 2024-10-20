51edd940489d3616633573fb69957671d422f781
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-20 10:10:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamsFlatMapExample.java
+++ src/main/java/modern/java8/streams/StreamsFlatMapExample.java
@@ -3,6 +3,7 @@ package modern.java8.streams;
 import modern.java8.data.Student;
 import modern.java8.data.StudentDataBase;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,5 +20,10 @@ public class StreamsFlatMapExample {
 
     public static void main(String[] args) {
         System.out.println("printStudentActivites :: " + printStudentActivities());
+
+        printStudentActivities().stream().distinct().forEach(System.out::println);
+
+        System.out.println(printStudentActivities().stream().distinct().count());
+
     }
 }
