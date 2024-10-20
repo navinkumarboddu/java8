c2a8ffec30163299fe3171471c54092e0796d389
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-20 10:15:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamsComparatorExample.java
+++ src/main/java/modern/java8/streams/StreamsComparatorExample.java
@@ -16,9 +16,20 @@ public class StreamsComparatorExample {
                 .collect(Collectors.toList());
     }
 
+    public static List<Student> sortStudentsByGpa(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .sorted(Comparator.comparing(Student::getGpa))
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
         //sortByName
         System.out.println("Students sorted by Name:");
         sortStudentsByName().forEach(System.out::println);
+
+        //sortByGpa
+        System.out.println("Students sorted by Gpa:");
+        sortStudentsByGpa().forEach(System.out::println);
     }
 }
