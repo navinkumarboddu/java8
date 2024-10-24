772282d83e2e63fa8d0724671c9f1e00ca735e9b
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-24 10:30:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamMapReduceExample.java
+++ src/main/java/modern/java8/streams/StreamMapReduceExample.java
@@ -9,9 +9,22 @@ public class StreamMapReduceExample {
         return StudentDataBase.getAllStudents()
                 .stream()
                 .map(Student::getNoteBooks)
-                .reduce(0,(a,b) -> a+b);
+                //.reduce(0,(a,b) -> a+b);
+                .reduce(0, Integer::sum);
     }
+
+    private static int noOfNoteBooksOfGradeAbove3(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .filter(s -> s.getGradeLevel() >= 3)
+                .map(Student::getNoteBooks)
+                //.reduce(0,(a,b) -> a+b);
+                .reduce(0, Integer::sum);
+    }
+
     public static void main(String[] args) {
         System.out.println("No of Books of all students: " + noOfNoteBooks());
+
+        System.out.println("No of Books of all students of Grade above 3: " + noOfNoteBooksOfGradeAbove3());
     }
 }
