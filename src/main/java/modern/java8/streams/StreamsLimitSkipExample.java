2bd3d621fa77b8853aed2f022ed69d9065032dcd
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-24 10:50:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamsLimitSkipExample.java
+++ src/main/java/modern/java8/streams/StreamsLimitSkipExample.java
@@ -12,8 +12,16 @@ public class StreamsLimitSkipExample {
                 .reduce((a,b) -> a + b);
     }
 
+    public static Optional<Integer> skip(List<Integer> integerList){
+        return integerList.stream()
+                .skip(3)
+                .reduce((a,b) -> a + b);
+    }
+
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(6,7,8,9,10);
         System.out.println("Sum of Limit:" + limit(integerList).get());
+
+        System.out.println("Sum of skip:" + skip(integerList).get());
     }
 }
