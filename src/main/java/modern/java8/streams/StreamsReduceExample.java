e39b4a1fb2bc862be8ddd265fdfd409580ca60ce
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-24 10:20:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamsReduceExample.java
+++ src/main/java/modern/java8/streams/StreamsReduceExample.java
@@ -1,5 +1,9 @@
 package modern.java8.streams;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +21,12 @@ public class StreamsReduceExample {
                 .reduce((a,b) -> a*b);
     }
 
+    public static Optional<Student> getHighestGpaStudent(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .reduce((s1,s2) ->  (s1.getGpa() > s2.getGpa()) ? s1 : s2);
+    }
+
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1,3,5,7);
         System.out.println("Multiplication ::" + performMulitplication(integers));
@@ -30,6 +40,11 @@ public class StreamsReduceExample {
         System.out.println(result1.isPresent());
         if(result1.isPresent()){
             System.out.println(result1.get());
+        }
+
+        Optional<Student> studentOptional = getHighestGpaStudent();
+        if(studentOptional.isPresent()){
+            System.out.println(studentOptional.get().getName());
         }
     }
 }
