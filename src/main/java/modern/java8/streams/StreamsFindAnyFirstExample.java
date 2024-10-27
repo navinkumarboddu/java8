ccb38685636e31c71b974ebd85560f6226fec7ce
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-27 10:10:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamsFindAnyFirstExample.java
+++ src/main/java/modern/java8/streams/StreamsFindAnyFirstExample.java
@@ -13,10 +13,23 @@ public class StreamsFindAnyFirstExample {
                 .findAny();
     }
 
+    public static Optional<Student> findFirstStudent(){
+        return StudentDataBase.getAllStudents().stream()
+                .filter(s -> s.getGpa() >= 4.1)
+                .findFirst();
+    }
+
     public static void main(String[] args) {
         Optional<Student> studentOptionalFindAny = findAnyStudent();
         if(studentOptionalFindAny.isPresent()){
             System.out.println("Found the student : " + studentOptionalFindAny.get());
+        } else {
+            System.out.println("Student not found");
+        }
+
+        Optional<Student> studentOptionalFindFirst = findFirstStudent();
+        if(studentOptionalFindFirst.isPresent()){
+            System.out.println("Found the student : " + studentOptionalFindFirst.get());
         } else {
             System.out.println("Student not found");
         }
