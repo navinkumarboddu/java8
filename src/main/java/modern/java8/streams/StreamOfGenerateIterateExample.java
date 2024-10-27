c89901bccca964f9ffef680bcf1080064380ea49
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-27 10:25:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamOfGenerateIterateExample.java
+++ src/main/java/modern/java8/streams/StreamOfGenerateIterateExample.java
@@ -1,5 +1,7 @@
 package modern.java8.streams;
 
+import java.util.Random;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class StreamOfGenerateIterateExample {
@@ -9,6 +11,11 @@ public class StreamOfGenerateIterateExample {
         stringStream.forEach(System.out::println);
 
         Stream.iterate(1, x -> x * 2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        Supplier<Integer> integerSupplier = new Random():: nextInt;
+        Stream.generate(integerSupplier)
                 .limit(10)
                 .forEach(System.out::println);
     }
