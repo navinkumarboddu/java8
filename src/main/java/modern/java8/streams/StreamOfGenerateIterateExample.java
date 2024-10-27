a2a474b4b3905f1e650dbcc817ce57fac852d42a
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-27 10:20:00 +0530

    added files

--- src/main/java/modern/java8/streams/StreamOfGenerateIterateExample.java
+++ src/main/java/modern/java8/streams/StreamOfGenerateIterateExample.java
@@ -7,5 +7,9 @@ public class StreamOfGenerateIterateExample {
     public static void main(String[] args) {
         Stream<String> stringStream = Stream.of("navin","ramesh","ganesh");
         stringStream.forEach(System.out::println);
+
+        Stream.iterate(1, x -> x * 2)
+                .limit(10)
+                .forEach(System.out::println);
     }
 }
