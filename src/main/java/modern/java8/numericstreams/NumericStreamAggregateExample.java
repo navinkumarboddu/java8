e45fdcb35ad2a568ad7d9cf4d8f9ef4471f58416
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-28 10:15:00 +0530

    added files

--- src/main/java/modern/java8/numericstreams/NumericStreamAggregateExample.java
+++ src/main/java/modern/java8/numericstreams/NumericStreamAggregateExample.java
@@ -1,5 +1,7 @@
 package modern.java8.numericstreams;
 
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 public class NumericStreamAggregateExample {
@@ -7,5 +9,12 @@ public class NumericStreamAggregateExample {
     public static void main(String[] args) {
         int sum = IntStream.rangeClosed(1,50).sum();
         System.out.println("Sum is : " + sum);
+
+        OptionalInt optionalInt = IntStream.rangeClosed(0,0).max();
+
+        System.out.println(IntStream.rangeClosed(0,0).count());
+
+        System.out.println(optionalInt.isPresent() ? optionalInt.getAsInt() : 0);
+        //System.out.println("Max value is :" + optionalInt);
     }
 }
