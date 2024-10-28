8d300301e195a63b07b15bd3ac6834877c42f72a
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-28 10:05:00 +0530

    added files

--- src/main/java/modern/java8/numericstreams/NumericStreamsRangeExample.java
+++ src/main/java/modern/java8/numericstreams/NumericStreamsRangeExample.java
@@ -1,6 +1,8 @@
 package modern.java8.numericstreams;
 
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class NumericStreamsRangeExample {
 
@@ -8,9 +10,16 @@ public class NumericStreamsRangeExample {
         IntStream intStream = IntStream.range(1,50);
         System.out.println("Range Count :" + intStream.count());
 
-        IntStream.range(1,50).forEach(v -> System.out.println(v + ","));
-
+        IntStream.range(1,50).forEach(v -> System.out.print(v + ","));
+        System.out.println();
         System.out.println("RangeClosed Count :" + IntStream.rangeClosed(1,50).count());
-        IntStream.rangeClosed(1,50).forEach(v -> System.out.println(v + ","));
+        IntStream.rangeClosed(1,50).forEach(v -> System.out.print(v + ","));
+        System.out.println();
+        System.out.println("Long Stream Range Closed Count :" + IntStream.rangeClosed(1,50).count());
+        LongStream.rangeClosed(1,50).forEach(v -> System.out.print(v + ","));
+
+        System.out.println();
+        System.out.println("Double Stream Range using IntStream Count :" + IntStream.rangeClosed(1,50).count());
+        IntStream.range(1,50).asDoubleStream().forEach(v -> System.out.print(v + ","));
     }
 }
