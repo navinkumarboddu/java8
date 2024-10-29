18761c577c1c9648bc0ccdf7f1d948993feffd5d
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-29 10:00:00 +0530

    added files

--- src/main/java/modern/java8/numericstreams/NumericStreamsMapToExample.java
+++ src/main/java/modern/java8/numericstreams/NumericStreamsMapToExample.java
@@ -1,4 +1,19 @@
 package modern.java8.numericstreams;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class NumericStreamsMapToExample {
+
+    public static List<Integer> mapToObj(){
+        return IntStream.rangeClosed(1,5)
+                .mapToObj((i) -> {
+                    return new Integer(i);
+                }).collect(Collectors.toList()) ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("mapToObj : " + mapToObj());
+    }
 }
