a51e3a4d11bc1ce258321086d999a7571b33e2e2
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-29 10:15:00 +0530

    added files

--- src/main/java/modern/java8/streams_terminal/StreamsJoiningExample.java
+++ src/main/java/modern/java8/streams_terminal/StreamsJoiningExample.java
@@ -1,4 +1,35 @@
 package modern.java8.streams_terminal;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.stream.Collectors;
+
 public class StreamsJoiningExample {
+
+    public static String joining_1(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(Collectors.joining());
+    }
+
+    public static String joining_2(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(Collectors.joining("-"));
+    }
+
+    public static String joining_3(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getName)
+                .collect(Collectors.joining("-","(",")"));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("joining_1 : " + joining_1());
+        System.out.println("joining_2 : " + joining_2());
+    }
 }
