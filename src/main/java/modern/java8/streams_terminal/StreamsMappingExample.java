26941f73575faacf9ca257cec243291bd400a55c
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-30 10:10:00 +0530

    added files

--- src/main/java/modern/java8/streams_terminal/StreamsMappingExample.java
+++ src/main/java/modern/java8/streams_terminal/StreamsMappingExample.java
@@ -1,4 +1,18 @@
 package modern.java8.streams_terminal;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamsMappingExample {
+
+    public static void main(String[] args) {
+        List<String> namesList =
+        StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.mapping(Student::getName,Collectors.toList()));
+        System.out.println("namesList : " + namesList);
+    }
 }
