bc563cb691bd7ef7dce490e6a58b2dcecb819afa
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-30 10:00:00 +0530

    added files

--- src/main/java/modern/java8/streams_terminal/StreamsCountingExample.java
+++ src/main/java/modern/java8/streams_terminal/StreamsCountingExample.java
@@ -1,4 +1,18 @@
 package modern.java8.streams_terminal;
 
+import modern.java8.data.StudentDataBase;
+
+import java.util.stream.Collectors;
+
 public class StreamsCountingExample {
+
+    public static long count(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.counting());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("count : " + count());
+    }
 }
