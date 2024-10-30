50880d50c78c98a71836dfda52994d34ff670915
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-30 10:25:00 +0530

    added files

--- src/main/java/modern/java8/streams_terminal/StreamsMaxMinByExample.java
+++ src/main/java/modern/java8/streams_terminal/StreamsMaxMinByExample.java
@@ -1,4 +1,20 @@
 package modern.java8.streams_terminal;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class StreamsMaxMinByExample {
+
+    public static Optional<Student> minBy_Example(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.minBy(Comparator.comparing(Student::getGpa)));
+    }
+
+    public static void main(String[] args) {
+    }
 }
