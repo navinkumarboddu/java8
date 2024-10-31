a6cf76c2912c93760706ff563f95a5d3e379e63d
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-31 10:45:00 +0530

    added files

--- src/main/java/modern/java8/streams_terminal/StreamsSumAverageExample.java
+++ src/main/java/modern/java8/streams_terminal/StreamsSumAverageExample.java
@@ -1,4 +1,19 @@
 package modern.java8.streams_terminal;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.stream.Collectors;
+
 public class StreamsSumAverageExample {
+
+    public static int sum(){
+        return StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.summingInt(Student::getNoteBooks));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum of all notebooks : " + sum());
+    }
 }
