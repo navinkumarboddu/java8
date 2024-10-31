e7f370ecf9bc34c8548573819a03a3078811abad
Navinkumar Boddu <swarmrobots@gmail.com>
2024-10-31 11:00:00 +0530

    added files

--- src/main/java/modern/java8/streams_terminal/StreamsGroupingByExample.java
+++ src/main/java/modern/java8/streams_terminal/StreamsGroupingByExample.java
@@ -1,8 +1,22 @@
 package modern.java8.streams_terminal;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamsGroupingByExample {
 
-    public static void main(String[] args) {
+    public static void groupStudentsByGender(){
+        Map<String,List<Student>> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGender));
+        System.out.println(studentMap);
+    }
 
+    public static void main(String[] args) {
+        groupStudentsByGender();
     }
 }
