9e01242a67e56e5b28f8e03aa6974ebd147e949b
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-02 11:00:00 +0530

    added files

--- src/main/java/modern/java8/streams_terminal/StreamsGroupingByExample.java
+++ src/main/java/modern/java8/streams_terminal/StreamsGroupingByExample.java
@@ -3,6 +3,7 @@ package modern.java8.streams_terminal;
 import modern.java8.data.Student;
 import modern.java8.data.StudentDataBase;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,8 +24,28 @@ public class StreamsGroupingByExample {
         System.out.println(studentMap);
     }
 
+    public static void twoLevelGrouping_1(){
+        Map<Integer, Map<String,List<Student>>> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGradeLevel,
+                        Collectors.groupingBy(student -> student.getGpa() >= 3.8 ? "Outstanding" : "Average")));
+
+        System.out.println(studentMap);
+    }
+
+    public static void twoLevelGrouping_2(){
+        Map<Integer, Integer> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.groupingBy(Student::getGradeLevel,
+                        Collectors.summingInt(Student::getNoteBooks)));
+
+        System.out.println(studentMap);
+    }
+
     public static void main(String[] args) {
-        groupStudentsByGender();
-        customizedGroupBy();
+        //groupStudentsByGender();
+        //customizedGroupBy();
+        //twoLevelGrouping_1();
+        twoLevelGrouping_2();
     }
 }
