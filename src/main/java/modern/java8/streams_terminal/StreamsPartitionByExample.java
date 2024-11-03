e1f5e4875a6221305498380aebc905283669a4d4
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-03 11:00:00 +0530

    added files

---
+++ src/main/java/modern/java8/streams_terminal/StreamsPartitionByExample.java
@@ -0,0 +1,25 @@
+package modern.java8.streams_terminal;
+
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamsPartitionByExample {
+
+    public static void partitioningBy_1(){
+        Predicate<Student> gpaPredicate = student -> student.getGpa() >= 3.8;
+
+        Map<Boolean, List<Student>> partitioningMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(Collectors.partitioningBy(gpaPredicate));
+
+        System.out.println("partitioningMap : " + partitioningMap);
+    }
+    public static void main(String[] args) {
+        partitioningBy_1();
+    }
+}
