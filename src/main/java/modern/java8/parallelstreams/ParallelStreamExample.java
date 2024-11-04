eba0669a9f9265039393699399463838ebfb08df
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-04 11:00:00 +0530

    added files

--- src/main/java/modern/java8/parallelstreams/ParallelStreamExample.java
+++ src/main/java/modern/java8/parallelstreams/ParallelStreamExample.java
@@ -1,4 +1,22 @@
 package modern.java8.parallelstreams;
 
+import java.util.stream.IntStream;
+
 public class ParallelStreamExample {
+
+    public static int sum_sequential_Stream(){
+        return IntStream.rangeClosed(1,100000)
+                .sum();
+    }
+
+    public static int sum_parallel_Stream(){
+        return IntStream.rangeClosed(1,100000)
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum_sequential_Stream());
+
+        System.out.println(sum_parallel_Stream());
+    }
 }
