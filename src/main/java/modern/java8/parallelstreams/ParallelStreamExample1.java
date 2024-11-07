cfc308882049f21390e30bb7a4df27de1b3a064c
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-07 11:10:00 +0530

    added files

--- src/main/java/modern/java8/parallelstreams/ParallelStreamExample1.java
+++ src/main/java/modern/java8/parallelstreams/ParallelStreamExample1.java
@@ -11,12 +11,15 @@ public class ParallelStreamExample1 {
     public static List<String> sequentialPrintStudentActivities(){
         long startTime = System.currentTimeMillis();
         List<String> studentActivities =
-                StudentDataBase.getAllStudents().stream() //Stream<Student>
+                StudentDataBase.getAllStudents()
+                        .stream() //Stream<Student>
                         .map(Student::getActivities) //Stream<List<String>>
                         .flatMap(List::stream) //Stream<String>
+                        .distinct()
+                        .sorted()
                         .collect(Collectors.toList());
         long endTime = System.currentTimeMillis();
-        System.out.println("Duration to execute the pipeline : " + (endTime - startTime));
+        System.out.println("Duration to execute the pipeline in sequential : " + (endTime - startTime));
         return studentActivities;
     }
 
@@ -28,13 +31,16 @@ public class ParallelStreamExample1 {
                         .parallel()
                         .map(Student::getActivities) //Stream<List<String>>
                         .flatMap(List::stream) //Stream<String>
+                        .distinct()
+                        .sorted()
                         .collect(Collectors.toList());
         long endTime = System.currentTimeMillis();
-        System.out.println("Duration to execute the pipeline : " + (endTime - startTime))
+        System.out.println("Duration to execute the pipeline in parallel : " + (endTime - startTime));
         return studentActivities;
     }
 
     public static void main(String[] args) {
-
+        sequentialPrintStudentActivities();
+        parallelPrintStudentActivities();
     }
 }
