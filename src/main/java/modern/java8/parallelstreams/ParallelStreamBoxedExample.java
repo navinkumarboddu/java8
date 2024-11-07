438414024d12780cce92104177174bc1a5f3bc86
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-07 11:20:00 +0530

    added files

--- src/main/java/modern/java8/parallelstreams/ParallelStreamBoxedExample.java
+++ src/main/java/modern/java8/parallelstreams/ParallelStreamBoxedExample.java
@@ -1,12 +1,22 @@
 package modern.java8.parallelstreams;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ParallelStreamBoxedExample {
 
+    public static int sequentialSum(List<Integer> integerList){
+        long startTime = System.currentTimeMillis();
+        int sum = integerList.stream()
+                .reduce(0, (x,y) -> x + y);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Duration to execute the sequentialSum : " + (endTime - startTime));
+        return sum;
+    }
+
     public static void main(String[] args) {
-        IntStream.rangeClosed(1,10000)
+        List<Integer> integerList = IntStream.rangeClosed(1,10000)
                 .boxed()
                 .collect(Collectors.toList());
     }
