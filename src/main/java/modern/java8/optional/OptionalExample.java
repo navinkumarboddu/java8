474d4a7ad104930230b37dbf48c1fb608975f651
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-10 11:15:00 +0530

    added files

--- src/main/java/modern/java8/optional/OptionalExample.java
+++ src/main/java/modern/java8/optional/OptionalExample.java
@@ -1,4 +1,21 @@
 package modern.java8.optional;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
 public class OptionalExample {
+
+    public static String getStudentName(){
+         //Student student = StudentDataBase.studentSupplier.get();
+         Student student = null;
+         if(student != null){
+             return student.getName();
+         }
+         return null;
+    }
+
+    public static void main(String[] args) {
+        String name = getStudentName();
+        System.out.println("Length of the student name : " + name.length());
+    }
 }
