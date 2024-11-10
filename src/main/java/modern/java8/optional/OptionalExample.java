f61bf97680e82429cb56266a699a7083f389e028
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-10 11:25:00 +0530

    added files

--- src/main/java/modern/java8/optional/OptionalExample.java
+++ src/main/java/modern/java8/optional/OptionalExample.java
@@ -8,24 +8,27 @@ import java.util.Optional;
 public class OptionalExample {
 
     public static String getStudentName(){
-         //Student student = StudentDataBase.studentSupplier.get();
-         Student student = null;
+         Student student = StudentDataBase.studentSupplier.get();
+         //Student student = null;
          if(student != null){
              return student.getName();
          }
          return null;
     }
 
-    public static String getStudentNameOptional(){
+    public static Optional<String> getStudentNameOptional(){
         Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get());
         if(studentOptional.isPresent()){
-            return studentOptional.get().getName();
+            return studentOptional.map(Student::getName);
         }
-        return null;
+        return Optional.empty();
     }
 
     public static void main(String[] args) {
         String name = getStudentName();
         System.out.println("Length of the student name : " + name.length());
+
+        Optional<String> studentOptional = getStudentNameOptional();
+        System.out.println("Length of the student name : " + studentOptional.get().length());
     }
 }
