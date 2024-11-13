7ed6fda60b335e549a95a7c244b200de0d647df2
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-13 11:30:00 +0530

    added files

--- src/main/java/modern/java8/optional/OptionalOfEmptyNullableExample.java
+++ src/main/java/modern/java8/optional/OptionalOfEmptyNullableExample.java
@@ -1,8 +1,14 @@
 package modern.java8.optional;
 
+import java.util.Optional;
+
 public class OptionalOfEmptyNullableExample {
 
-    public static void main(String[] args) {
+    public static Optional<String> ofNullable(){
+        return Optional.ofNullable("Hello");
+    }
 
+    public static void main(String[] args) {
+        System.out.println(ofNullable());
     }
 }
