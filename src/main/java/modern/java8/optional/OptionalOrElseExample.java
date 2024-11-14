86cdb9ebc716439fbcf713a76e5fb5be0c291cba
Navinkumar Boddu <swarmrobots@gmail.com>
2024-11-14 11:50:00 +0530

    added files

--- src/main/java/modern/java8/optional/OptionalOrElseExample.java
+++ src/main/java/modern/java8/optional/OptionalOrElseExample.java
@@ -1,9 +1,20 @@
 package modern.java8.optional;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public class OptionalOrElseExample {
 
+    public static String optionalOrElse(){
+        Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get());
+        String name = studentOptional.map(Student::getName).orElse("Default");
+        return name;
+    }
 
     public static void main(String[] args) {
-
+        System.out.println(optionalOrElse());
     }
 }
