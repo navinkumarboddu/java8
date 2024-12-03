f4fda73e333b431c5cdbfe247f0bd6af59042b4d
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-03 11:20:00 +0530

    added files

--- src/main/java/modern/java8/optional/OptionalMapFlatMapExample.java
+++ src/main/java/modern/java8/optional/OptionalMapFlatMapExample.java
@@ -1,8 +1,21 @@
 package modern.java8.optional;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.Optional;
+
 public class OptionalMapFlatMapExample {
 
-    public static void main(String[] args) {
+    //filter
+    public static void optionalFilter(){
+        Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get());
+        studentOptional.
+                filter(student -> student.getGpa()>=3.5)
+                .ifPresent(student -> System.out.println(student));
+    }
 
+    public static void main(String[] args) {
+        optionalFilter();
     }
 }
