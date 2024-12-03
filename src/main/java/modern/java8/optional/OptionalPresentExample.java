09153754484d8107b9437c44c9a299519e919b5f
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-03 11:00:00 +0530

    added files

--- src/main/java/modern/java8/optional/OptionalPresentExample.java
+++ src/main/java/modern/java8/optional/OptionalPresentExample.java
@@ -1,8 +1,15 @@
 package modern.java8.optional;
 
+import java.util.Optional;
+
 public class OptionalPresentExample {
 
-    public static void main(String[] args) {
+    public static void isPresent(){
+        Optional<String> optional = Optional.ofNullable("Hello");
+        System.out.println(optional.isPresent());
+    }
 
+    public static void main(String[] args) {
+        isPresent();
     }
 }
