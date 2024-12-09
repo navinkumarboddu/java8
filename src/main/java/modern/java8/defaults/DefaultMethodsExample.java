c0bcdda2763738d2c0d17472ce434727fe5010bd
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-09 11:00:00 +0530

    added files

--- src/main/java/modern/java8/defaults/DefaultMethodsExample.java
+++ src/main/java/modern/java8/defaults/DefaultMethodsExample.java
@@ -1,8 +1,19 @@
 package modern.java8.defaults;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DefaultMethodsExample {
 
     public static void main(String[] args) {
 
+        /**
+         * Sort the list names in alphabetical order
+         */
+        List<String> stringList = Arrays.asList("Namesh","Duggu","Nimish","Suresh","Ramesh");
+
+        /**
+         * Prior Java 8
+         */
     }
 }
