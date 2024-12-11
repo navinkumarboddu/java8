b02c608bba2773a9cdec035768c48b10ff2fd1ec
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-11 11:00:00 +0530

    added files

--- src/main/java/modern/java8/defaults/DefaultMethodsExample.java
+++ src/main/java/modern/java8/defaults/DefaultMethodsExample.java
@@ -2,6 +2,7 @@ package modern.java8.defaults;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class DefaultMethodsExample {
@@ -16,7 +17,13 @@ public class DefaultMethodsExample {
         /**
          * Prior Java 8
          */
-        Collections.sort(stringList);
-        System.out.println("Sort List using Collections.Sort() : " + stringList);
+        //Collections.sort(stringList);
+        //System.out.println("Sort List using Collections.Sort() : " + stringList);
+
+        /**
+         * Java 8
+         */
+        stringList.sort(Comparator.naturalOrder());
+        System.out.println("Sort List using List.Sort() : " + stringList);
     }
 }
