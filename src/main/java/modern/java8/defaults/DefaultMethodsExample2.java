0704c9a68f7b9c6fb9f5de493b3f58f373c114ea
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-11 11:20:00 +0530

    added files

--- src/main/java/modern/java8/defaults/DefaultMethodsExample2.java
+++ src/main/java/modern/java8/defaults/DefaultMethodsExample2.java
@@ -3,6 +3,7 @@ package modern.java8.defaults;
 import modern.java8.data.Student;
 import modern.java8.data.StudentDataBase;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -10,10 +11,21 @@ public class DefaultMethodsExample2 {
 
     static Consumer<Student> studentConsumer = (student -> System.out.println(student));
 
+    public static void sortByName(List<Student> studentList){
+        System.out.println("After sort");
+        Comparator<Student> nameComparator = Comparator.comparing(Student::getName);
+        studentList.sort(nameComparator);
+
+        studentList.forEach(studentConsumer);
+    }
+
     public static void main(String[] args) {
         List<Student> studentList = StudentDataBase.getAllStudents();
         //System.out.println(studentList);
 
+        System.out.println("Before sort");
         studentList.forEach(studentConsumer);
+
+        sortByName(studentList);
     }
 }
