754578899f0beb598e0444abd32a051ceeb46fa9
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-11 11:15:00 +0530

    added files

--- src/main/java/modern/java8/defaults/DefaultMethodsExample2.java
+++ src/main/java/modern/java8/defaults/DefaultMethodsExample2.java
@@ -1,8 +1,19 @@
 package modern.java8.defaults;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.Consumer;
+
 public class DefaultMethodsExample2 {
 
-    public static void main(String[] args) {
+    static Consumer<Student> studentConsumer = (student -> System.out.println(student));
 
+    public static void main(String[] args) {
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        //System.out.println(studentList);
+
+        studentList.forEach(studentConsumer);
     }
 }
