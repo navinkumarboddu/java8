a4349972d9d31dfeb982b25b60cac676212725dd
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-13 11:00:00 +0530

    added files

--- src/main/java/modern/java8/defaults/DefaultMethodsExample3.java
+++ src/main/java/modern/java8/defaults/DefaultMethodsExample3.java
@@ -1,8 +1,23 @@
 package modern.java8.defaults;
 
+import modern.java8.data.Student;
+import modern.java8.data.StudentDataBase;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class DefaultMethodsExample3 {
 
-    public static void main(String[] args) {
+    public static void sortWithNullValues(List<Student> studentList){
+        Comparator<Student> studentComparator = Comparator.nullsFirst(DefaultMethodsExample2.nameComparator);
+    }
 
+    public static void main(String[] args) {
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        System.out.println("Before sort");
+
+        //sortByName(studentList);
+        //sortByGpa(studentList);
     }
 }
