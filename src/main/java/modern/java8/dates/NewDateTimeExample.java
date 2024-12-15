bce10e00db9700f2f09431f4c80d09b16d1a347c
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-15 11:10:00 +0530

    added files

--- src/main/java/modern/java8/dates/NewDateTimeExample.java
+++ src/main/java/modern/java8/dates/NewDateTimeExample.java
@@ -1,6 +1,7 @@
 package modern.java8.dates;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class NewDateTimeExample {
 
@@ -8,7 +9,11 @@ public class NewDateTimeExample {
         //localDate
         LocalDate localDate = LocalDate.now();
         System.out.println("LocalDate : " + localDate);
+
         //localTime
+        LocalTime localTime = LocalTime.now();
+        System.out.println("LocalTime : " + localTime);
+
         //localDateTime
     }
 }
