280d6ad4919b0cc290abf6bf54b0383bbd4d1232
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-16 11:10:00 +0530

    added files

--- src/main/java/modern/java8/dates/LocalDateExample.java
+++ src/main/java/modern/java8/dates/LocalDateExample.java
@@ -11,5 +11,14 @@ public class LocalDateExample {
         System.out.println("CustomDate : " + LocalDate.of(2018,07,17));
 
         System.out.println("Day of Year : " + LocalDate.ofYearDay(2018,365));
+
+        /**
+         * Get values from LocalDate
+         */
+        System.out.println(localDate.getMonth());
+        System.out.println(localDate.getDayOfMonth());
+        System.out.println(localDate.getDayOfWeek());
+        System.out.println(localDate.getDayOfYear());
+
     }
 }
