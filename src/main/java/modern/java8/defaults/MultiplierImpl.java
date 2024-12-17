fb6baa64a1dc6d4500a2da4cd04318e7c5c03f94
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-17 11:00:00 +0530

    added files

--- src/main/java/modern/java8/defaults/MultiplierImpl.java
+++ src/main/java/modern/java8/defaults/MultiplierImpl.java
@@ -1,2 +1,12 @@
-package modern.java8.defaults;public class MultiplierImpl {
+package modern.java8.defaults;
+
+import java.util.List;
+
+public class MultiplierImpl implements Multiplier {
+    @Override
+    public int multiply(List<Integer> integerList) {
+        return integerList
+                .stream()
+                .reduce(1,(x,y) -> x*y);
+    }
 }
