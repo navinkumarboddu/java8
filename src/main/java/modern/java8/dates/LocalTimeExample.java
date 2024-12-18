77dee6e7695da56d5d4848a0d7bfd41f16316df4
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-18 11:05:00 +0530

    added files

---
+++ src/main/java/modern/java8/dates/LocalTimeExample.java
@@ -0,0 +1,11 @@
+package modern.java8.dates;
+
+import java.time.LocalTime;
+
+public class LocalTimeExample {
+
+    public static void main(String[] args) {
+        LocalTime localTime = LocalTime.now();
+        System.out.println("LocalTime : " + localTime);
+    }
+}
