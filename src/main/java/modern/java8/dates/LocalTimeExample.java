3e653450a2b65137afde5c7ef78843bcbcde4676
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-18 11:15:00 +0530

    added files

--- src/main/java/modern/java8/dates/LocalTimeExample.java
+++ src/main/java/modern/java8/dates/LocalTimeExample.java
@@ -10,5 +10,11 @@ public class LocalTimeExample {
 
         LocalTime localTime1 = LocalTime.of(23,23);
         System.out.println("LocalTime1 : " + localTime1);
+
+        LocalTime localTime2 = LocalTime.of(23,33,33);
+        System.out.println("LocalTime2 : " + localTime2);
+
+        LocalTime localTime3 = LocalTime.of(23,33,33,980980980);
+        System.out.println("LocalTime3 : " + localTime3);
     }
 }
