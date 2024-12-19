dde3aae3d4c87ce86633696e465272de8b30cc7d
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-19 11:25:00 +0530

    added files

--- src/main/java/modern/java8/dates/LocalDateTimeExample.java
+++ src/main/java/modern/java8/dates/LocalDateTimeExample.java
@@ -7,5 +7,8 @@ public class LocalDateTimeExample {
     public static void main(String[] args) {
         LocalDateTime localDateTime = LocalDateTime.now();
         System.out.println("LocalDateTime : " + localDateTime);
+
+        LocalDateTime localDateTime1 = LocalDateTime.of(2025, 03, 01, 05, 05, 05, 978);
+        System.out.println("LocalDateTime1 : " + localDateTime1);
     }
 }
