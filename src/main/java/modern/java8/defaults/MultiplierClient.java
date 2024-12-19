707df24bc42db080e54c26db62b91c6a8a81ba40
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-19 11:00:00 +0530

    added files

--- src/main/java/modern/java8/defaults/MultiplierClient.java
+++ src/main/java/modern/java8/defaults/MultiplierClient.java
@@ -1,8 +1,13 @@
 package modern.java8.defaults;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MultiplierClient {
 
     public static void main(String[] args) {
-
+        Multiplier multiplier = new MultiplierImpl();
+        List<Integer> integerList = Arrays.asList(1,3,5);
+        System.out.println("Multiplication : " + multiplier.multiply(integerList));
     }
 }
