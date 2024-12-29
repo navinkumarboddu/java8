98c90f1d55d2adc878ca0baec813f2c2b7040d21
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-29 11:00:00 +0530

    added files

--- src/main/java/modern/java8/defaults/Client123.java
+++ src/main/java/modern/java8/defaults/Client123.java
@@ -1,4 +1,8 @@
 package modern.java8.defaults;
 
-public class Client123 {
+public class Client123 implements Interface1, Interface2, Interface3 {
+
+    public static void main(String[] args) {
+
+    }
 }
