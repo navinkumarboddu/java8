57b7dffeaf595a905d505872daed580fbd499cd7
Navinkumar Boddu <swarmrobots@gmail.com>
2024-12-31 11:00:00 +0530

    added files

--- src/main/java/modern/java8/defaults/Interface2.java
+++ src/main/java/modern/java8/defaults/Interface2.java
@@ -1,6 +1,6 @@
 package modern.java8.defaults;
 
-public interface Interface2 {
+public interface Interface2 extends Interface1{
 
     default void methodB(){
         System.out.println("Inside Method B");
--- src/main/java/modern/java8/defaults/Interface3.java
+++ src/main/java/modern/java8/defaults/Interface3.java
@@ -1,6 +1,6 @@
 package modern.java8.defaults;
 
-public interface Interface3 {
+public interface Interface3 extends Interface2 {
 
     default void methodC(){
         System.out.println("Inside Method C");
