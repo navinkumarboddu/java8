d29be99ecee374e5995fff901aaf5c49ba58ae15
Navinkumar Boddu <swarmrobots@gmail.com>
2025-01-05 11:20:00 +0530

    added files

--- src/main/java/modern/java8/dates/LocalDateTimeExample.java
+++ src/main/java/modern/java8/dates/LocalDateTimeExample.java
@@ -3,6 +3,7 @@ package modern.java8.dates;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
 
 public class LocalDateTimeExample {
 
@@ -15,5 +16,20 @@ public class LocalDateTimeExample {
 
         LocalDateTime localDateTime2 = LocalDateTime.of(LocalDate.now(), LocalTime.now());
         System.out.println("LocalDateTime2 : " + localDateTime2);
+
+        /**
+         * Getting time and date from LocalDateTime instance
+         */
+        System.out.println("Hour : " + localDateTime.getHour());
+        System.out.println("Minutes : " + localDateTime.getMinute());
+        System.out.println("Day : " + localDateTime.getDayOfMonth());
+        System.out.println("Day : " + localDateTime.get(ChronoField.DAY_OF_MONTH));
+
+        /**
+         * Modifying LocalDateTime
+         */
+        System.out.println("plus hour : " + localDateTime.plusHours(2));
+        System.out.println("minus Days : " + localDateTime.minusDays(2));
+        System.out.println("withMonth : " + localDateTime.withMonth(2));
     }
 }
