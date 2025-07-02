d99dc20ea1b96eaa5df81ba63ea39e10777135be
Navinkumar Boddu <swarmrobots@gmail.com>
2025-07-02 11:20:02 +0530

    updated file - added exaple where LVTI is not allowed

--- src/main/java/modern/v2/var/VarTypeExample.java
+++ src/main/java/modern/v2/var/VarTypeExample.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 public class VarTypeExample {
 
+    //var in the class properties is not allowed
+    //private var x = "abc";
+
     public static void main(String[] args) {
         var list = List.of("nibhish","navin");
         System.out.println("list = " + list);
@@ -20,8 +23,19 @@ public class VarTypeExample {
 
         var transformedName = transform("navin");
         System.out.println(transformedName);
+
+        //Limitations
+        //var x = null;
+
+        //var x = "ABC";
+        //x=5; //Changing the type is not allowed
+
+        var var = "Java"; //is allowed
+
     }
 
+    //var in the function parameters is not allowed
+    //static String transform(var name) // not allowed
     static String transform(String name){
         return name.toUpperCase();
     }
