b5634044407a08cbd1c6bfa6ba502dcd0a5b7e84
Navinkumar Boddu <swarmrobots@gmail.com>
2026-03-10 10:00:00 +0530

    added files

--- src/main/java/com/java8/practice/dsa/search/linear/FindMin.java
+++ src/main/java/com/java8/practice/dsa/search/linear/FindMin.java
@@ -1,4 +1,21 @@
 package com.java8.practice.dsa.search.linear;
 
 public class FindMin {
+
+    public static void main(String[] args) {
+        int[] arr = {18,12,-7, 3, 14,21};
+        System.out.println(min(arr));
+    }
+
+    private static int min(int[] arr) {
+        int min = arr[0];
+        int count = 1;
+        while(count < arr.length-1) {
+            if(arr[count] < min){
+                min = arr[count];
+            }
+            count++;
+        }
+        return min;
+    }
 }
