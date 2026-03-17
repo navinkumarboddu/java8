338c7743d73149a2c9228456d606543dc27880b9
Navinkumar Boddu <swarmrobots@gmail.com>
2026-03-17 19:11:22 +0530

    added files

--- src/main/java/com/java8/practice/dsa/search/linear/SearchInStrings.java
+++ src/main/java/com/java8/practice/dsa/search/linear/SearchInStrings.java
@@ -1,4 +1,27 @@
 package com.java8.practice.dsa.search.linear;
 
 public class SearchInStrings {
+    public static void main(String[] args) {
+        String str = "This is beatiful world";
+        char[] s = str.toCharArray();
+        char target = 'x';
+        if(search(s,target)){
+            System.out.println("Found character '" + target + "' in the string :" + str);
+        } else {
+            System.out.println("Character not found...");
+        }
+    }
+
+    static boolean search(char[] s, char target){
+        boolean found = false;
+        if(s.length != 0){
+            for (int index = 0; index < s.length - 1; index++) {
+                if(s[index] == target){
+                    found = true;
+                }
+            }
+        }
+        return found;
+    }
 }
+
