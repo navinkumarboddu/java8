b1a499448febccd79be69ce278a93a5a943cb98c
Navinkumar Boddu <swarmrobots@gmail.com>
2026-03-23 11:28:56 +0530

    added files

--- src/main/java/com/java8/practice/dsa/ReverseNumber.java
+++ src/main/java/com/java8/practice/dsa/ReverseNumber.java
@@ -24,4 +24,21 @@ public class ReverseNumber {
 
         System.out.println("Reverse Number :" + rev);
     }
+
+    private static int reverse(int x) {
+        boolean neg = false;
+        if(x <0){
+            neg = true;
+            x = x * -1;
+        }
+        int rev = 0;
+        while(x >0){
+            rev = (rev*10) + (x %10);
+            x = x /10;
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+        }
+        return (neg ? (rev * -1) : rev);
+    }
 }
